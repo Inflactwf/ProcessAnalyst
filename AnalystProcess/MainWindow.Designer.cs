3b5defacbc32fb46187e914f2a9e54ec0f66adc7
Inflactwf <enot5000@gmail.com>
2021-11-13 15:12:07 +0200

    Добавьте файлы проекта.

---
+++ AnalystProcess/MainWindow.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace AnalystProcess
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.StartBtn = new System.Windows.Forms.Button();
+            this.LogBox = new System.Windows.Forms.RichTextBox();
+            this.CheckDisk = new System.Windows.Forms.Button();
+            this.DrivesList = new System.Windows.Forms.ComboBox();
+            this.FirstWorker = new System.ComponentModel.BackgroundWorker();
+            this.FirstProgress = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // StartBtn
+            // 
+            this.StartBtn.BackColor = System.Drawing.SystemColors.Control;
+            this.StartBtn.Font = new System.Drawing.Font("Segoe UI Light", 10F);
+            this.StartBtn.ForeColor = System.Drawing.Color.Black;
+            this.StartBtn.Location = new System.Drawing.Point(289, 205);
+            this.StartBtn.Name = "StartBtn";
+            this.StartBtn.Size = new System.Drawing.Size(100, 47);
+            this.StartBtn.TabIndex = 0;
+            this.StartBtn.TabStop = false;
+            this.StartBtn.Text = "Начать";
+            this.StartBtn.UseVisualStyleBackColor = false;
+            this.StartBtn.Click += new System.EventHandler(this.StartBtn_Click);
+            // 
+            // LogBox
+            // 
+            this.LogBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.LogBox.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LogBox.Location = new System.Drawing.Point(12, 12);
+            this.LogBox.Name = "LogBox";
+            this.LogBox.ReadOnly = true;
+            this.LogBox.Size = new System.Drawing.Size(376, 191);
+            this.LogBox.TabIndex = 0;
+            this.LogBox.TabStop = false;
+            this.LogBox.Text = "";
+            // 
+            // CheckDisk
+            // 
+            this.CheckDisk.BackColor = System.Drawing.SystemColors.Control;
+            this.CheckDisk.Enabled = false;
+            this.CheckDisk.Font = new System.Drawing.Font("Segoe UI", 7F);
+            this.CheckDisk.ForeColor = System.Drawing.Color.Black;
+            this.CheckDisk.Location = new System.Drawing.Point(103, 205);
+            this.CheckDisk.Name = "CheckDisk";
+            this.CheckDisk.Size = new System.Drawing.Size(82, 23);
+            this.CheckDisk.TabIndex = 2;
+            this.CheckDisk.TabStop = false;
+            this.CheckDisk.Text = "Проверить";
+            this.CheckDisk.UseVisualStyleBackColor = false;
+            this.CheckDisk.Click += new System.EventHandler(this.CheckDisk_Click);
+            // 
+            // DrivesList
+            // 
+            this.DrivesList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.DrivesList.Enabled = false;
+            this.DrivesList.FormattingEnabled = true;
+            this.DrivesList.Location = new System.Drawing.Point(12, 206);
+            this.DrivesList.Name = "DrivesList";
+            this.DrivesList.Size = new System.Drawing.Size(82, 21);
+            this.DrivesList.TabIndex = 3;
+            // 
+            // FirstWorker
+            // 
+            this.FirstWorker.WorkerReportsProgress = true;
+            this.FirstWorker.WorkerSupportsCancellation = true;
+            this.FirstWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.FirstWorker_DoWork);
+            this.FirstWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.FirstWorker_RunWorkerCompleted);
+            // 
+            // FirstProgress
+            // 
+            this.FirstProgress.Location = new System.Drawing.Point(12, 229);
+            this.FirstProgress.MarqueeAnimationSpeed = 0;
+            this.FirstProgress.Name = "FirstProgress";
+            this.FirstProgress.Size = new System.Drawing.Size(172, 23);
+            this.FirstProgress.Step = 4;
+            this.FirstProgress.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.FirstProgress.TabIndex = 0;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.ClientSize = new System.Drawing.Size(400, 254);
+            this.Controls.Add(this.FirstProgress);
+            this.Controls.Add(this.DrivesList);
+            this.Controls.Add(this.CheckDisk);
+            this.Controls.Add(this.LogBox);
+            this.Controls.Add(this.StartBtn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Process Analyst";
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button StartBtn;
+        private System.Windows.Forms.RichTextBox LogBox;
+        private System.Windows.Forms.Button CheckDisk;
+        private System.Windows.Forms.ComboBox DrivesList;
+        private System.ComponentModel.BackgroundWorker FirstWorker;
+        private System.Windows.Forms.ProgressBar FirstProgress;
+    }
+}
+
---
+++ AnalystProcess/MainWindow.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace AnalystProcess
+{
+    public partial class MainWindow : Form
+    {
+        string CurrentDrive = @"C:\";
+        bool InternalError = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StartBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                StartBtn.Enabled = false;
+                string Firstname = "xampp-control";
+                string Secondname = "httpd";
+                Process[] FirstProc = Process.GetProcessesByName(Firstname);
+                Process[] SecondProc = Process.GetProcessesByName(Secondname);
+                foreach (Process anti in FirstProc)
+                {
+                    string FileLocation = Path.GetDirectoryName(anti.MainModule.FileName);
+
+                    if (FirstProc.Length != 0)
+                    {
+                        if (LogBox.Text.Length == 0)
+                        {
+                            LogBox.Text += "[" + DateTime.Now + "] " + Environment.NewLine + "Найден процесс: " + anti.ProcessName + " (" + FirstProc.Length + ")" + Environment.NewLine + "ID процесса: " + anti.Id + Environment.NewLine + "Время запуска процесса: " + anti.StartTime + Environment.NewLine + "Рабочая папка: " + FileLocation;
+                        }
+                        else
+                        {
+                            LogBox.Text += Environment.NewLine + Environment.NewLine + "[" + DateTime.Now + "] " + Environment.NewLine + "Найден процесс: " + anti.ProcessName + " (" + FirstProc.Length + ")" + Environment.NewLine + "ID процесса: " + anti.Id + Environment.NewLine + "Время запуска процесса: " + anti.StartTime + Environment.NewLine + "Рабочая папка: " + FileLocation;
+                        }
+
+                        if (File.Exists(FileLocation + @"\htdocs\lib\HTMLParser.php"))
+                        {
+                            FileInfo fi = new FileInfo(FileLocation + @"\htdocs\lib\HTMLParser.php");
+                            if (fi.Length == 55265)
+                            {
+                                LogBox.Text += Environment.NewLine + "Найден HTMLParser последней версии. [55265 bytes] (" + FileLocation + @"\htdocs" + ")";
+                                LogBox.Text += Environment.NewLine + "Дата создания: " + fi.CreationTime;
+                                LogBox.Text += Environment.NewLine + "Дата последнего открытия: " + fi.LastAccessTime;
+                                LogBox.Text += Environment.NewLine + "Дата последнего редактирования: " + fi.LastWriteTime;
+                                //Directory.Delete(FileLocation + @"\htdocs", true);
+                                LogBox.Text += Environment.NewLine + "Удаление HTMLParser завершено.";
+                                break;
+                            }
+                            else
+                            {
+                                LogBox.Text += Environment.NewLine + "Найдена одна из старых версий HTMLParser. (" + FileLocation + @"\htdocs" + ")";
+                                LogBox.Text += Environment.NewLine + "Дата создания: " + fi.CreationTime;
+                                LogBox.Text += Environment.NewLine + "Дата последнего открытия: " + fi.LastAccessTime;
+                                LogBox.Text += Environment.NewLine + "Дата последнего редактирования: " + fi.LastWriteTime;
+                            }
+                        }
+                    }
+                }
+
+                if (FirstProc.Length == 0)
+                {
+                    if (LogBox.Text.Length == 0)
+                    {
+                        LogBox.Text += "[" + DateTime.Now + "] " + Environment.NewLine + "Процессов с названием '" + Firstname + "' не найдено, проверяем Apache.";
+                    }
+                    else
+                    {
+                        LogBox.Text += Environment.NewLine + Environment.NewLine + "[" + DateTime.Now + "] " + Environment.NewLine + "Процессов с названием '" + Firstname + "' не найдено, проверяем Apache.";
+                    }
+
+                    foreach (Process secondanti in SecondProc)
+                    {
+                        string FileLocation = Path.GetDirectoryName(secondanti.MainModule.FileName);
+                        string FormatPath = Directory.GetParent(Directory.GetParent(FileLocation).ToString()).ToString();
+
+                        if (SecondProc.Length != 0)
+                        {
+                            if (LogBox.Text.Length == 0)
+                            {
+                                LogBox.Text += "[" + DateTime.Now + "] " + Environment.NewLine + "Найден процесс: " + secondanti.ProcessName + " (" + SecondProc.Length + ")" + Environment.NewLine + "ID процесса: " + secondanti.Id + Environment.NewLine + "Время запуска процесса: " + secondanti.StartTime + Environment.NewLine + "Рабочая папка: " + FileLocation;
+                            }
+                            else
+                            {
+                                LogBox.Text += Environment.NewLine + Environment.NewLine + "[" + DateTime.Now + "] " + Environment.NewLine + "Найден процесс: " + secondanti.ProcessName + " (" + SecondProc.Length + ")" + Environment.NewLine + "ID процесса: " + secondanti.Id + Environment.NewLine + "Время запуска процесса: " + secondanti.StartTime + Environment.NewLine + "Рабочая папка: " + FileLocation;
+                            }
+                        }
+
+                        if (File.Exists(FormatPath + @"\htdocs\lib\HTMLParser.php"))
+                        {
+                            FileInfo fi = new FileInfo(FormatPath + @"\htdocs\lib\HTMLParser.php");
+                            if (fi.Length == 55265)
+                            {
+                                LogBox.Text += Environment.NewLine + "Найден HTMLParser последней версии. [55265 bytes] (" + FormatPath + @"\htdocs" + ")";
+                                LogBox.Text += Environment.NewLine + "Дата создания: " + fi.CreationTime;
+                                LogBox.Text += Environment.NewLine + "Дата последнего открытия: " + fi.LastAccessTime;
+                                LogBox.Text += Environment.NewLine + "Дата последнего редактирования: " + fi.LastWriteTime;
+                                //Directory.Delete(FormatPath + @"\htdocs", true);
+                                LogBox.Text += Environment.NewLine + "Удаление HTMLParser завершено.";
+                                break;
+                            }
+                            else
+                            {
+                                LogBox.Text += Environment.NewLine + "Найдена одна из старых версий HTMLParser. (" + FormatPath + @"\htdocs" + ")";
+                                LogBox.Text += Environment.NewLine + "Дата создания: " + fi.CreationTime;
+                                LogBox.Text += Environment.NewLine + "Дата последнего открытия: " + fi.LastAccessTime;
+                                LogBox.Text += Environment.NewLine + "Дата последнего редактирования: " + fi.LastWriteTime;
+                            }
+                        }
+                    }
+
+                    if (SecondProc.Length == 0)
+                    {
+                        if (LogBox.Text.Length == 0)
+                        {
+                            LogBox.Text += "[" + DateTime.Now + "] " + Environment.NewLine + "Процессов с названием '" + Secondname + "' не найдено. Запуск поиска файла...";
+                        }
+                        else
+                        {
+                            LogBox.Text += Environment.NewLine + Environment.NewLine + "[" + DateTime.Now + "] " + Environment.NewLine + "Процессов с названием '" + Secondname + "' не найдено. Запуск поиска файла...";
+                        }
+
+                        FirstWorker.RunWorkerAsync();
+                    }
+                }
+
+                CheckDisk.Enabled = true;
+                DrivesList.Enabled = true;
+            }
+            catch { }
+        }
+
+        public void WalkDirectoryTree(DirectoryInfo root)
+        {
+            FileInfo[] files = null;
+            DirectoryInfo[] subDirs = null;
+
+            try
+            {
+                files = root.GetFiles("*.*");
+            }
+            catch { }
+
+            if (files != null)
+            {
+                subDirs = root.GetDirectories();
+                foreach (DirectoryInfo dirInfo in subDirs)
+                {
+                    WalkDirectoryTree(dirInfo);
+                }
+
+                foreach (FileInfo fi in files)
+                {
+                    if (fi.Name.Equals("HTMLParser.php"))
+                    {
+                        string FileLocation = fi.FullName;
+                        string DirLocation = Directory.GetParent(Path.GetDirectoryName(fi.FullName)).ToString();
+                        FileInfo info = new FileInfo(FileLocation);
+                        if (info.Length == 55265)
+                        {
+                            LogBox.Text += Environment.NewLine + Environment.NewLine + "[" + DateTime.Now + "]" + Environment.NewLine + "Найден HTMLParser последней версии. [55265 bytes] (" + DirLocation + ")";
+                            LogBox.Text += Environment.NewLine + "Дата создания: " + fi.CreationTime;
+                            LogBox.Text += Environment.NewLine + "Дата последнего открытия: " + fi.LastAccessTime;
+                            LogBox.Text += Environment.NewLine + "Дата последнего редактирования: " + fi.LastWriteTime;
+                            //Directory.Delete(DirLocation, true);
+                            LogBox.Text += Environment.NewLine + "Удаление HTMLParser завершено.";
+                            CheckDisk.Enabled = true;
+                            DrivesList.Enabled = true;
+                            break;
+                        }
+                        else
+                        {
+                            LogBox.Text += Environment.NewLine + Environment.NewLine + "[" + DateTime.Now + "]" + Environment.NewLine + "Найдена одна из старых версий HTMLParser. (" + DirLocation + ")";
+                            LogBox.Text += Environment.NewLine + "Дата создания: " + fi.CreationTime;
+                            LogBox.Text += Environment.NewLine + "Дата последнего открытия: " + fi.LastAccessTime;
+                            LogBox.Text += Environment.NewLine + "Дата последнего редактирования: " + fi.LastWriteTime;
+                            continue;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            DrivesList.Items.AddRange(Environment.GetLogicalDrives());
+            DrivesList.SelectedIndex = 0;
+        }
+
+        private void CheckDisk_Click(object sender, EventArgs e)
+        {
+            FirstWorker.RunWorkerAsync();
+        }
+
+        private void FirstWorker_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            FirstProgress.Style = ProgressBarStyle.Marquee;
+            FirstProgress.MarqueeAnimationSpeed = 1;
+            CheckDisk.Enabled = false;
+            DrivesList.Enabled = false;
+            CurrentDrive = DrivesList.SelectedItem.ToString();
+            DriveInfo di = new DriveInfo(CurrentDrive);
+            if (!di.IsReady)
+            {
+                InternalError = true;
+            }
+            else
+            {
+                InternalError = false;
+                DirectoryInfo rootDir = di.RootDirectory;
+                WalkDirectoryTree(rootDir);
+            }
+        }
+
+        private void FirstWorker_RunWorkerCompleted(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
+        {
+            FirstProgress.Style = ProgressBarStyle.Continuous;
+            FirstProgress.MarqueeAnimationSpeed = 0;
+            if(InternalError)
+            {
+                MessageBox.Show("Этот диск не может быть использован для чтения.");
+            }
+            else
+            {
+                MessageBox.Show("Проверка диска завершена.");
+            }
+            CheckDisk.Enabled = true;
+            DrivesList.Enabled = true;
+        }
+    }
+}
